18d7b2c2dc6f7ebb5d80834f8375b50af11bd9b5
Sofia <m.sofiasarhiri@gmail.com>
2024-04-05 21:57:35 -0500

    create server js, auth routes file, initalize proxy between servers.

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path'
+const app = express();
+// eslint-disable-next-line no-unused-vars
+
+const PORT = 3000;
+
+app.use(express.static(path.resolve('index.html')));
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
