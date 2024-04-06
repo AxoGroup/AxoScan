b3b6e70e2d9d24442ef2ff82bf9f3b77cf839eb5
Sean Ryan <sfryan95@gmail.com>
2024-04-06 11:27:00 -0700

    created Home page component with header, instruction card, drag and drop area, and footer with the design teams githubs. > > Co-authored-by: Dylan <dster777@gmail.com>

---
+++ src/components/Instructions.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card, Space } from 'antd';
+const App = () => (
+  <Space direction="vertical" size={16} style={{ border: 'none' }}>
+    <Card
+      title="Effortless Upload Guide"
+      // extra={<a href="#">More</a>}
+      style={{
+        width: 400,
+        height: 200,
+        marginBottom: 50,
+        backgroundColor: '#272727',
+        border: 'none',
+        color: 'rgba(255, 255, 255, 0.45)',
+      }}
+      headStyle={{ borderBottom: '1px solid #93d5a2', color: 'rgba(255, 255, 255, 0.65)' }}>
+      <p>Welcome to our streamlined upload process! To ensure your files are uploaded seamlessly, simply drag and drop them into the area below. </p>
+    </Card>
+  </Space>
+);
+export default App;
