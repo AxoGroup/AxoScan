d7efc119fb0ac5aee99e0ab7645b624aef21085b
Sofia <m.sofiasarhiri@gmail.com>
2024-04-06 17:41:28 -0500

    API connection finalized, built out get request to API with receiptController and searchArray middleware. Co-authored-by: Austin <alvarez.austin78100@gmail.com> Co-authored-by: Aiden <aidencarere@gmail.com>

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,22 @@
 import express from 'express';
 const router = express.Router();
 import cors from 'cors';
+
 // eslint-disable-next-line no-unused-vars
-import {test, registerUser, loginUser, getProfile, updateProfile, deleteAccount, isAuthenticated} from '../controllers/controllers'
+import receiptController from '../controllers/receiptControllers';
+import searchArray from '../controllers/searchArray';
 
 router.use(
     cors({
         credentials: true,
         origin: 'http://localhost:5173'
     })
-)
-
+);
 
 // post request
-// get 
+router.post('/upload', 
+receiptController.uploadReceipt, searchArray.searched, 
+(req, res) =>  res.status(200).json(res.locals.object));
+
+// eslint-disable-next-line no-undef
+export default router;
