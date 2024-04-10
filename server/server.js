d40335396a8539914d1f4e3d0c446fdc842a4d17
Sean Ryan <sfryan95@gmail.com>
2024-04-10 13:52:22 -0700

    added testing for mongodb create receipt document middleware

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 // eslint-disable-next-line no-unused-vars
 const PORT = 3000;
 
-// delcare routers
+// declare routers
 import uploadRouter from './routes/authRoutes.js';
 
 // handle parsing request body
@@ -17,13 +17,13 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 
 // app.use(express.static(path.resolve('index.html'));
 app.get('/', (req, res) => {
-    res.status(200).send(express.static(path.resolve('index.html')));
+  res.status(200).send(express.static(path.resolve('index.html')));
 });
 
 // route handlers
 app.use('/api', uploadRouter);
 
-  //catch-all route handler for any requests to an unknown route
+//catch-all route handler for any requests to an unknown route
 app.use('*', (req, res) => res.sendStatus(404));
 
 // global error handler
@@ -32,16 +32,14 @@ app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 500,
-    message: { err: 'An error occurred' }, 
-  }
+    message: { err: 'An error occurred' },
+  };
   let errorObj = Object.assign({}, defaultErr, err);
   res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
 
-
 // route handler to then send to middleware, then we can work ond atabase to connect / send data to databsae
-
