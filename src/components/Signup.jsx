08dce03b8b9157ad528d57bf00dd01e634abe9da
Stephen Yang <stephen.yang98@gmail.com>
2024-04-10 17:16:12 -0700

    worked on signup page and styling

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import { Flex } from 'antd';
+import '../styles/Signup.css';
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -52,21 +53,22 @@ const Signup = () => {
   return (
     <Flex direction="column" justify='center' align="center" style={{ height: '100vh' }}>
     
-    <div className='container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <h1>AxoScan</h1>
+    <div className='signup-container'>
+      <h1 className='title'>AxoScan</h1>
       <div className='signup-text-background-box'>
         <form onSubmit={signup} className='signup'>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <label>Username</label>
-            <input type='text' placeholder='   Username' value={data.username} onChange={(e) => setData({ ...data, username: e.target.value })} />
-            <label>Email</label>
-            <input type='email' placeholder='   Email Address' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
-            <label>Password</label>
-            <input type='password' placeholder='   Password' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
-            <label>Confirm Password</label>
-            <input type='password' placeholder='   Confirm Password' value={data.confirmPassword} onChange={(e) => setData({ ...data, confirmPassword: e.target.value })} />
-            <button type='submit' className="submit-btn">Submit</button>
-            <button type="button" className="account-exists-btn" onClick={() => navigate('/login')}>Already have an account?</button>
+          <div className='signup-form-container'>
+            <label className='signup-form-element'>Username</label>
+            <input className='signup-form-element' type='text' placeholder='Username' value={data.username} onChange={(e) => setData({ ...data, username: e.target.value })} />
+            <label className='signup-form-element'>Email</label>
+            <input className='signup-form-element' type='email' placeholder='Email Address' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+            <label className='signup-form-element'>Password</label>
+            <input className='signup-form-element' type='password' placeholder='Password' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+            <label className='signup-form-element'>Confirm Password</label>
+            <input className='signup-form-element' type='password' placeholder='Confirm Password' value={data.confirmPassword} onChange={(e) => setData({ ...data, confirmPassword: e.target.value })} />
+            <button type='submit' className="submit-btn">Sign Up</button>
+            <p className="signup-form-element">Already have an account?</p>
+            <button type="button" className="alr-reg-btn" onClick={() => navigate('/login')}>Log In</button>
           </div>
         </form>
       </div>
