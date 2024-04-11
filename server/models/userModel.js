bb65e595654848c190a83b788767af7a5fcd72bd
Heather Pfeiffer <heatherpfeiffer@Heathers-MacBook-Pro.local>
2024-04-11 18:33:00 -0400

    just for safe measure

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -16,6 +16,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: {type: String, require: true},
   password: {type: String, require: true},
+  
 });
 
 const User = mongoose.model('user', userSchema);
