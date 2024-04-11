04e2146e1254ac9c5bf72562bfc923b675306ea5
Haider Ali <haiderali@Haiders-MacBook-Air.local>
2024-04-10 17:12:11 -0700

    text rendering completed

--- test/react2.test.jsx
+++ test/react2.test.jsx
@@ -1,32 +1,63 @@
 import React from 'react'
 import assert from 'assert'
 import { render, cleanup, fireEvent, screen, waitFor, act } from '@testing-library/react';
-import { describe, expect, it, beforeAll, afterAll, test, vi } from 'vitest'; // Import test from vitest
-//import userEvent from '@testing-library/user-event';
-// import sinon from 'sinon';
-import UploadButton from '../src/components/UploadButton.jsx'
+import { describe, expect, it, beforeAll, afterAll, test, vi } from 'vitest';
 
+import UploadButton from '../src/components/UploadButton.jsx'
+import Instructions from '../src/components/Instructions.jsx'
+import TitleHeader from '../src/components/TitleHeader.jsx'
+import DragAndDrop from '../src/components/DragAndDrop.jsx'
+import Footer from '../src/components/Footer.jsx'
 
 test('wew', () => {
 	assert(1, 1);
 });
 
 describe('UploadButton', () => {
-	test('should invoke handleClick when button is clicked', () => {
-		// Arrange
+	test('button should have uploadButton text', () => {
+		render(<UploadButton/>);
+		expect(screen.getByRole('button', { name: 'Back to Upload' })).toBeInTheDocument();
 
+	});
+	test('should invoke handleClick when button is clicked', () => {
 		const handleClick = () => {
 			console.log('I was called');
 		}
-
-		const mock = vi.fn().mockImplementation(handleClick) // Create a spy on handleClick function  //vi.fn or vi.SpyOn
+		const mock = vi.fn().mockImplementation(handleClick);
 		render(<UploadButton handleClick={mock} />);
 
-		// Act
-		fireEvent.click(screen.getByText('Back to Upload')); // Simulate a click on the button
-
-		// Assert
-		//assert(handleClick.toHaveBeenCalledTimes(1), true); // Check if handleClick was called once
+		fireEvent.click(screen.getByText('Back to Upload')); 
 		expect(mock).toHaveBeenCalledTimes(1);
 	});
 });
+
+describe('Expected text to display on the page', () => {
+	test('instructions display on page', () => {
+		render(<Instructions/>);
+		const instructionsText = 'Welcome to our streamlined upload process! To ensure your files are uploaded seamlessly, simply drag and drop them into the area below.'
+		expect(screen.getByText(instructionsText)).toBeInTheDocument();
+		expect(screen.getByText('Effortless Upload Guide')).toBeInTheDocument();
+
+	});
+	test('heading renders', () => {
+		render(<TitleHeader/>);
+		const headingText = 'AxoScan';
+		expect(screen.getByText(headingText)).toBeInTheDocument();
+	});
+	test('drag and drop text renders', () => {
+		render(<DragAndDrop/>);
+		const dragText = 'Support for a single upload. Strictly prohibited from uploading company data or other banned files.';
+		expect(screen.getByText(dragText)).toBeInTheDocument();
+	});
+	test('footer text renders', () => {
+		render(<Footer/>);
+
+		expect(screen.getByText('AxoGroup')).toBeInTheDocument();
+		expect(screen.getByText('Design Team:')).toBeInTheDocument();
+		expect(screen.getByText('Sofia')).toBeInTheDocument();
+		expect(screen.getByText('Aiden')).toBeInTheDocument();
+		expect(screen.getByText('Austin')).toBeInTheDocument();
+		expect(screen.getByText('Sean')).toBeInTheDocument();
+		expect(screen.getByText('Dylan')).toBeInTheDocument();
+	})
+})
